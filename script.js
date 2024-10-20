5464e127ce00e8b37d4f31c5e029bdac1649cc9c
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-10-20 12:37:09 +0200

    feat: add functions for add, subtract, multiply, and divide

---
+++ script.js
@@ -0,0 +1,20 @@
+const add = function (num1, num2) {
+  return num1 + num2;
+};
+
+const subtract = function (num1, num2) {
+  return num1 - num2;
+};
+
+const multiply = function (num1, num2) {
+  return num1 * num2;
+};
+
+const divide = function (num1, num2) {
+  return num1 / num2;
+};
+
+console.log(add(1, 2));
+console.log(subtract(1, 2));
+console.log(multiply(1, 2));
+console.log(divide(1, 2));
