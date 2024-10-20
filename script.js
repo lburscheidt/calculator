11287023bf167a4a62eadb4713175f69075c102b
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-10-20 12:47:10 +0200

    feat: add function "operate" that calls one of the other four functions depending on operator

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ let num1;
 let num2;
 let operator;
 
+function operate(num1, num2, operator) {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "*":
+      return multiply(num1, num2);
+    case "/":
+      return divide(num1, num2);
+  }
+}
+
 const add = function (num1, num2) {
   return num1 + num2;
 };
@@ -18,7 +31,7 @@ const divide = function (num1, num2) {
   return num1 / num2;
 };
 
-console.log(add(1, 2));
-console.log(subtract(1, 2));
-console.log(multiply(1, 2));
-console.log(divide(1, 2));
+console.log(operate(1, 2, "+"));
+console.log(operate(1, 2, "-"));
+console.log(operate(1, 2, "*"));
+console.log(operate(1, 2, "/"));
